63713bb1216c946d58572de2fb6e2dbde1bc197c
lolomeira <138534956+lolomeira@users.noreply.github.com>
2024-10-22 22:03:08 -0300

    aula 23/10

---
+++ MissaoVisaoValores.js
@@ -0,0 +1,18 @@
+criaCartao (
+  'Missão',
+  'Saiba qual é a missao de nossa empresa!',
+  'Nossa missão é, trazer alegria para as pessoas através de momentos de qualidade em familia, tomando uma caana bem gelada com um pastel frito na hora!!!'
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual  a visão de nossa empresa',
+    'Nossa visao é servir refeições com agilidade, mas garantindo a qualidade, para que os clientes possam saborear o seu pedido ainda quente e estejam sempre satisfeitos com o sabor e qualidade do produto'
+)
+
+criaCartao (
+    'Valores',
+    'Saiba quais sao os valores de nossa empresa!',
+    'Nossos valores sao, solidariedade, qualidade, excelencia, inovação, sensações e a fidelizaçao do cliente'
+)
+
