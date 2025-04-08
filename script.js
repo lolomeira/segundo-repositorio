a747d2ac147f37fe3445eab401fbf91aa6945ac3
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-04-07 22:51:44 -0300

    aula 07-04

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao = document.getElementById('aumentarfonte');
+    aumentaFonteBotao.addEventListener('click', function(){
+
+
+
+       })
+       const botaoDeAcessibilidade = document.getElementById
+       ('botao-acessibilidade');
+       const opcoesDeAcessibilidadade = document.getElementById
+       (opcoes-acessibilidade);
+       
+    }) 
