e605bd653eccb49a7b4221d47bbbc164bc968a82
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-05-13 22:14:40 -0300

    teste

--- script.js
+++ script.js
@@ -20,6 +20,6 @@ document.addEventListener('DOMContentLoaded', function () {
    })
    diminuiFonteBotao.addEventListener('click', function(){
        tamanhoAtualFonte -= 0.1;
-       document.body.style.fontSize = '${tamanhoAtualFonte}rem'
+       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
    })
 })
