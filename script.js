356d205978d8349745111062a3c61f9a8e7d40f3
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-05-29 22:47:02 -0300

    AULA 29/05

--- script.js
+++ script.js
@@ -12,14 +12,19 @@ document.addEventListener('DOMContentLoaded', function () {
    
    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+    const alternaContrate = document.getElementById('alterna-contraste');
 
    let tamanhoAtualFonte = 1;
    aumentaFonteBotao.addEventListener('click', function(){
        tamanhoAtualFonte += 0.1;
-       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+       document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
    })
    diminuiFonteBotao.addEventListener('click', function(){
        tamanhoAtualFonte -= 0.1;
-       document.body.style.fontSize = `${tamanhoAtualFonte}rem`
+       document.body.style.fontSize = `${tamanhoAtualFonte}rem;
+    `
+   })
+   alternaContrate.addEventListener('click', function(){
+    document.body.classList.toggle('alto-contraste');
    })
 })
