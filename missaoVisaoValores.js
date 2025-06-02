1c6b4e8e588468664c2b68a930c071097aa32644
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-06-02 12:30:52 -0300

    teste

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,17 +1,17 @@
 criaCartao (
   'GERSON',
-  'Saiba qual é a missao de nossa empresa!',
+  'Mesmo com vc gritando com agente!!',
   'Os melhores professores são aqueles que se tornam verdadeiros amigos e compartilham sua sabedoria em todos os momentos'
 )
 
 criaCartao (
     'CESAR',
-    'Saiba qual  a visão de nossa empresa',
+    'Sbemos que no fundo vc nos ama!!',
     'Queremos homenagear todos esses anos que vc nos fez passar raiva com (Java Script, CSS, HTML, Arduíno R3 e códigos e mais códigos..'
 )
 
 criaCartao (
     'GROBE',
-    'Saiba quais sao os valores de nossa empresa!',
+    'Sua turma favorita!!',
     'Nossa turma te ama e não poderiamos deixar de homenagear uma das pessoas q mais participou do  nossoe nsino médio esperamos que voce aceite nosso convite... ')
 
