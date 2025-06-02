09c64c3ffa079df829578108c0f892dc220a3c79
lolomeira <138534956+lolomeira@users.noreply.github.com>
2025-06-02 12:35:10 -0300

    teste

--- missaoVisaoValores.js
+++ missaoVisaoValores.js
@@ -1,17 +1,17 @@
 criaCartao (
   'GERSON',
-  'Mesmo com vc gritando com agente!!',
+  'Mesmo com vc gritando com a gente!!',
   'Os melhores professores são aqueles que se tornam verdadeiros amigos e compartilham sua sabedoria em todos os momentos'
 )
 
 criaCartao (
     'CESAR',
-    'Sbemos que no fundo vc nos ama!!',
-    'Queremos homenagear todos esses anos que vc nos fez passar raiva com (Java Script, CSS, HTML, Arduíno R3 e códigos e mais códigos..'
+    'Sabemos que no fundo vc nos ama!!',
+    'Queremos homenagear todos esses anos que vc nos fez passar raiva com (Java Script, CSS, HTML, Arduíno R3) e códigos e mais códigos..'
 )
 
 criaCartao (
     'GROBE',
     'Sua turma favorita!!',
-    'Nossa turma te ama e não poderiamos deixar de homenagear uma das pessoas q mais participou do  nossoe nsino médio esperamos que voce aceite nosso convite... ')
+    'Nossa turma te ama e não poderiamos deixar de homenagear uma das pessoas q mais participou do  nosso ensino médio, e esperamos que voce aceite nosso convite... ')
 
